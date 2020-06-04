f880a27e4d1f940df2941d6233913be1ee7b4492
Misbah Ahmad <imiiucian@gmail.com>
2020-06-04 19:46:33 +0600

    init remote git

---
+++ i_am_rich/android/app/src/main/kotlin/com/misbah/iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.misbah.iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ quizzler_app/android/app/src/main/kotlin/com/misbah/quizzlerapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.misbah.quizzlerapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
